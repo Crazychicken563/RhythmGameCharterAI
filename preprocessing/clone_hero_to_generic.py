7a04e90ed161057efd24f0205872c85f82af3502
Seva Luchianov <seva.luchianov@everykey.com>
2019-11-03 19:39:52 -0500

    Had an idea for bpm detection, then found an existing implementation of that idea. Still is not accurate

--- preprocessing/clone_hero_to_generic.py
+++ preprocessing/clone_hero_to_generic.py
@@ -34,18 +34,27 @@ def main():
                             duration = datums[2].strip()
                             if augment <= 4:
                                 endNote = str(int(timestamp) + int(duration))
+                                # mnd will always be defined by this point since scanningHeader
+                                # can never be true without mnd being instantiated
                                 mnd.write(f"{timestamp} {str(augment)} {timestamp} {endNote}\n")
+                            else:
+                                # augments over 4 denote a unique type of note / note modifier
+                                # augment 7 means that the previous note has star power.
+                                # other augments currently unknown...
+                                if augment == 7:
+                                    # How should we write a star power not modifier to our generic data?
+                                    print("star power for duration: " + duration)
                 else:
                     if any(header in currLine for header in ["[ExpertSingle]", "[HardSingle]", "[MediumSingle]", "[EasySingle]"]):
                         print("Now scanning " + currLine)
                         notes.readline() #Skip the "{"
                         scanningHeader = True
-
-                        this_difficulty_file = os.path.join("clone_hero_data", currLine + "_" + name + ".mnd")
+                        mergedPathIntoName = name.replace("\\", "_")
+                        this_difficulty_file = os.path.join("clone_hero_data\\output", currLine + "_" + mergedPathIntoName + ".mnd")
                         print(this_difficulty_file)
                         success = False
-                        mnd = open(this_difficulty_file, mode="w", encoding="utf-8")
-                
+                        mnd = open(this_difficulty_file, mode="w+", encoding="utf-8")
+
                 currLine = notes.readline().strip()
 
 main()
