0bdaa8c0b220fd9773a522acf654cfc85998937d
Tristan Maidment <email@theedman.com>
2019-11-05 00:52:45 -0500

    trying to fix dumb github error

--- BeatSaber/preprocessing.py
+++ BeatSaber/preprocessing.py
@@ -4,6 +4,7 @@ from os.path import isfile, join, isdir
 import cv2
 import soundfile as sf
 import csv
+import pickle as pkl
 
 def load_songs(path):
     songs = [f for f in listdir(path) if isdir(join(path, f))]
@@ -37,25 +38,65 @@ def load_songs(path):
             
     return init_songs
 
-if __name__ == "__main__":
+def main(directory):
     path = './CustomLevels'
     songs = load_songs(path)
-    with open('songs.csv', 'w') as csvfile:
-        fieldnames = ['song', 'info', 'maps']
-        csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        for song in songs:
-            print(songs[song])
-            info = songs[song]
-            # read the sound file
-            data, samplerate = sf.read(info['egg'])
-            #print(data, samplerate)
-            # read the choreography 
-            maps = []
-            for map in info['maps']:
-                with open(map, 'r') as f:
-                    line = f.readline()
-                    choreo = eval(line)
-                    maps.append(choreo)
-                    #for key in choreo.keys():
-                    #    print(choreo[key])
-            csvwriter.writerow({'song':song, 'info':info, 'maps':maps})
+    for song in songs:
+        print(songs[song])
+        
+        info = songs[song]
+        # read the sound file
+        data, samplerate = sf.read(info['egg'])
+        #print(data, samplerate)
+        # read the choreography 
+        #maps = []
+        #obstacles = []
+        for map in info['maps']:
+            with open(map, 'r') as f:
+                line = f.readline()
+                choreo = eval(line)
+                #maps.append(choreo['_events'])
+                #obstacles.append(choreo['_obstacles'])
+                name = map.split('/')[-1]
+                with open((directory + song + name + '.pkl').replace(' ', ''), 'wb') as f:
+                    sample = {'name':song}
+                    sample['songdata'] = np.asarray(data)
+                    sample['samplerate'] = samplerate
+                    sample['lights'] = choreo['_events']
+                    sample['obstacles'] = choreo['_obstacles']
+                    sample['notes'] = choreo['_notes']
+                    pkl.dump(sample, f)
+
+                #for key in choreo.keys():
+                #    print(choreo[key])
+
+def main2(directory):
+    files = [join(directory, f) for f in listdir(directory) if isfile(join(directory, f))]
+    for file in files:
+        with open(file, 'rb') as f:
+            ## NOTE For reconstruction, this data needs to be added to *items*
+            sample = pkl.load(f)
+            name = sample['name'] # this is needed for reconstruction of the song
+            data = sample['songdata']
+            samplerate = sample['samplerate']
+            songlength = data.shape[0]/samplerate
+            notes = sample['notes']
+            print(name, samplerate)
+            #lights = sample['lights']
+            #obstacles = sample['obstacles'] # to be added in the future
+            for time in range(int(songlength)):
+                window = data[samplerate*time:samplerate*(time+1)]
+                label = np.zeros([window.shape[0], 5, 3, 2, 5])
+                for entry in notes:
+                    if entry['_time'] >= time and entry['_time'] < time + 1 and entry['_type'] < 3:
+                        # bandied fix for the weird omnidirectional boxes (8) - don't want a massive tensor, could set to index 4
+                        if (entry['_cutDirection']) > 3:
+                            entry['_cutDirection'] = 4
+                        label[int((entry['_time'] % 1) * samplerate), entry['_lineIndex'], entry['_lineLayer'], entry['_type'], entry['_cutDirection']] = 1
+                        with open('./samples_window/' + file.replace('.pkl','') + str(time) + '.pkl', 'rb') as f:
+                            pkl.dump({'name':name, 'time':entry['_time'], 'window':window, 'label':label})
+
+if __name__ == "__main__":
+    directory = './samples/'
+    main(directory)
+    main2(directory) #second preprocessing needed 
