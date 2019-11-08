fix prediction normalization, add files to gitignore

ddrmodel/ddr_model_test.py
@@ -19,7 +19,7 @@ LSTM_HISTORY_LENGTH = 48
 if len(sys.argv) > 1:
   path = sys.argv[1]
 else:
-  path = "../preprocessing/ddr_data/In The Groove/Anubis/c6_5820.mnd"
+  path = "../preprocessing/ddr_data/In The Groove/Anubis/c8_520.mnd"
 
 np.set_printoptions(precision=4,suppress=True)
 
@@ -32,7 +32,7 @@ def beat_find(time_point):
 raw_data = []
 with open(path, mode='r', encoding="utf-8") as generic:
     songpath = generic.readline()
-    bpm = generic.readline()
+    bpm = float(generic.readline())
     offset = generic.readline()
     for line in generic:
         line_data = line.split()
@@ -42,16 +42,18 @@ lrjumps = 0
 udjumps = 0
 timepoints = []
 history = [np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])]*LSTM_HISTORY_LENGTH
+notehistory = []
 last_time = 0
 for i in range(len(raw_data)):
     (time_point, note, start_long, end_long) = raw_data[i]
     next_time = raw_data[i+1][0] if i+1 < len(raw_data) else time_point+(192*5)
     beat_fract = beat_find(time_point)
-    mnd_in = np.array([time_point-last_time, next_time-time_point, beat_fract, note, start_long, end_long, bpm],dtype="float32",ndmin=2)
+    mnd_in = [(time_point-last_time)/(192*5), (next_time-time_point)/(192*5), beat_fract/48, note/4, start_long/4, end_long/4, bpm/400]
+    parsed_mnd_in = np.array(mnd_in,dtype="float32",ndmin=2)
     last_time = time_point
     hs = len(history)
     hist_in = np.array(history[hs-LSTM_HISTORY_LENGTH:hs],dtype="float32",ndmin=3)
-    probs = model.predict([mnd_in,hist_in])
+    probs = model.predict([np.array(parsed_mnd_in),hist_in])
     (l,u,d,r) = (x[0].astype('float') for x in probs)
     available = {'l':l,'u':u,'d':d,'r':r}
     out = {'l':0,'u':0,'d':0,'r':0}
@@ -66,11 +68,11 @@ for i in range(len(raw_data)):
           out[chosen] = i
           del available[chosen]
     (L,U,D,R) = (out['l'],out['u'],out['d'],out['r'])
-    print(mnd_in,[L,U,D,R])
-    #print(list(x[0].astype('float') for x in probs))
-    hist_data = [(time_point-last_time)/(192*5), (next_time-time_point)/(192*5), beat_fract/48, note/4, start_long/4, end_long/4, bpm/400]
-    hist_data.extend([L,U,D,R]/3)
-    history.append(hist_data)
+    #print([L,U,D,R],mnd_in)
+    print([L,U,D,R],list(x[0].astype('float') for x in probs))
+    mnd_in.extend([L/3,U/3,D/3,R/3])
+    history.append(mnd_in)
+    notehistory.append([L,U,D,R])
     timepoints.append(time_point)
     if (note+start_long == 2):
         jumps += 1
@@ -88,7 +90,7 @@ with open("output.sm", mode='w', encoding="utf-8") as out:
             (l,u,d,r) = (0,0,0,0)
             t = curr_t+192
         else:
-            (l,u,d,r) = history[LSTM_HISTORY_LENGTH+i][7:11]
+            (l,u,d,r) = notehistory[i]
             t = timepoints[i]
         if (curr_t+192 <= t):
             #blank measures
@@ -99,7 +101,7 @@ with open("output.sm", mode='w', encoding="utf-8") as out:
             for beat_offset in range(192//buf_res):
                 tmp = buf_notes.get(beat_offset*buf_res+curr_t, "0000")
                 out.write(tmp+"\n")
-                print(tmp,beat_offset*buf_res+curr_t)
+                #print(tmp,beat_offset*buf_res+curr_t)
             out.write(";\n" if (i == len(timepoints)) else ",\n")
             #clear current
             buf_res = 48
@@ -108,5 +110,5 @@ with open("output.sm", mode='w', encoding="utf-8") as out:
         buf_notes[t] = f"{l}{u}{d}{r}"
         res = beat_find(t)
         buf_res = min(res, buf_res)
-        print(l,u,d,r,t,beat_find(t),res)
+        #print(l,u,d,r,t,beat_find(t),res)
 print("Done!")

ddrmodel/ddr_model_train.py
@@ -1,4 +1,3 @@
-#BAAM
 import os
 import re
 
@@ -100,13 +99,14 @@ def mnd_data(chart):
 LSTM_HISTORY_LENGTH = 48
 
 mnd_input = layers.Input(shape=(7,),name="mnd_input")
-x = layers.Dense(24)(mnd_input)
+x = layers.Dense(32)(mnd_input)
 hist_input = layers.Input(shape=(LSTM_HISTORY_LENGTH,11,),name="hist_input")
-hist_dense = layers.TimeDistributed(Dense(16))(hist_input)
-hist_lstm = layers.LSTM(48)(hist_dense)
+hist_dense = layers.TimeDistributed(Dense(32))(hist_input)
+hist_dense2 = layers.TimeDistributed(Dense(32))(hist_dense)
+hist_lstm = layers.LSTM(32)(hist_dense2)
 x = layers.concatenate([x,hist_lstm])
 x = layers.Dense(32)(x)
-x = layers.Dense(24)(x)
+x = layers.Dense(32)(x)
 outL = layers.Dense(4, activation='softmax', name = "outL")(x)
 outU = layers.Dense(4, activation='softmax', name = "outU")(x)
 outD = layers.Dense(4, activation='softmax', name = "outD")(x)
@@ -121,6 +121,10 @@ model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 if len(sys.argv) > 1:
     if sys.argv[1] == "LOAD":
         model = load_model("ddr_model.h5")
+        #If loading from disk, assume the model is reasonably-trained and use the slower but "better" SGD algorithm
+        optimizer = keras.optimizers.SGD(nesterov=True)
+        model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+        
 
 def beat_find(time_point):
     for tt in [48, 24, 16, 12, 8, 6, 4, 3, 2, 1]: #time_resolution of individual notes
@@ -148,7 +152,20 @@ def generate_song_inout_data(mnd_arr, out_arr, bpm):
         mnd_data.extend(np.argmax(out_arr[pos],axis=1)/3)
         inputs.append(np.array(mnd_data))
         outputs.append(out_arr[pos])
-        yield ((input_mnd_aux, np.array(inputs[pos:i])), np.array(outputs[i]))
+        in_arr = np.array(inputs[pos:i])
+        out_dat = np.array(outputs[i])
+        #data augmentation: flip L/R, U/D, and both
+        yield ((input_mnd_aux, in_arr), out_dat)
+        out_dat[[0,3]] = out_dat[[3,0]]
+        in_arr[:,[7,10]] = in_arr[:,[10,7]]
+        yield ((input_mnd_aux, in_arr), out_dat)
+        out_dat[[1,2]] = out_dat[[1,2]]
+        in_arr[:,[8,9]] = in_arr[:,[8,9]]
+        yield ((input_mnd_aux, in_arr), out_dat)
+        out_dat[[0,3]] = out_dat[[3,0]]
+        in_arr[:,[7,10]] = in_arr[:,[10,7]]
+        yield ((input_mnd_aux, in_arr), out_dat)
+        
 
 def generate_dataset(n = sys.maxsize):
     gen = song_data()
@@ -161,6 +178,8 @@ def generate_dataset(n = sys.maxsize):
     return dataset
 
 def huge_full_dataset():
+    #load dataset from disk (from many files into one)
+    #may need modification once I add audio data
     if os.path.exists("ddr_dataset.p"):
         print("loading dataset")
         dataset = pickle.load(open("ddr_dataset.p","rb"))
@@ -176,6 +195,8 @@ def huge_full_dataset():
     outR_set = []
     bag = []
     max = len(dataset)
+    #Creates "super-batches" due to memory limitations
+    #Currently sized for ~8GB of usable memory)
     while True:
         if len(bag) == 0:
             print("reloading dataset bag: size="+str(max))