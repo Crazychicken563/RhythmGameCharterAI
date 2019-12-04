63b614c28ad0ccf174e1e21b25c1fcb20b9b5427
Seva Luchianov <vvl2@case.edu>
2019-12-04 18:30:14 -0500

    it ran. it crashed.

--- CloneHero/prototype.py
+++ CloneHero/prototype.py
@@ -28,86 +28,99 @@ torch.backends.cudnn.benchmark = True
 class network(nn.Module):
     def __init__(self):
         super(network, self).__init__()
-        self.fc1 = nn.Linear(88200, 22050)
-        self.fc2 = nn.Linear(22050, 8820)
-        self.fc3 = nn.Linear(8820, 5880)
-        self.fc4 = nn.Linear(5880, 4410)
-        self.fc5 = nn.Linear(4410, 2200)
-        self.fc6 = nn.Linear(2200, 1024)
-        self.fc7 = nn.Linear(1024, 512)
-        self.fc8 = nn.Linear(512, 256)
-        self.fc9 = nn.Linear(256, 148)
+        self.conv1 = nn.Conv2d(1, 10, (3,2), 2)
+        self.conv2 = nn.Conv2d(10, 10, (3,1), 2)
+        self.conv3 = nn.Conv2d(10, 10, (3,1), 2)
+        self.conv4 = nn.Conv2d(10, 10, (3,1), 2)
+        self.conv5 = nn.Conv2d(10, 10, (3,1), 2)
+        self.conv6 = nn.Conv2d(10, 10, (3,1), 2)
+        self.conv7 = nn.Conv2d(10, 10, (3,1), 2)
+        self.conv8 = nn.Conv2d(10, 10, (3,1), 2)
+        self.conv9 = nn.Conv2d(10, 10, (3,1), 2)
+        self.conv10 = nn.Conv2d(10, 10, (3,1), 2)
+        self.fc1 = nn.Linear(420, 300)
+        self.fc2 = nn.Linear(300, 200) 
+        self.fc3 = nn.Linear(200, 148)
 
     def forward(self, x):
-        x = x.view(-1, 88200)
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = torch.relu(self.conv3(x))
+        x = torch.relu(self.conv4(x))
+        x = torch.relu(self.conv5(x))
+        x = torch.relu(self.conv6(x))
+        x = torch.relu(self.conv7(x))
+        x = torch.relu(self.conv8(x))
+        x = torch.relu(self.conv9(x))
+        x = torch.relu(self.conv10(x))
+        x = x.view(-1, 1, 420)
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
-        x = torch.relu(self.fc4(x))
-        x = torch.relu(self.fc5(x))
-        x = torch.relu(self.fc6(x))
-        x = torch.relu(self.fc7(x))
-        x = torch.relu(self.fc8(x))
-        x = torch.relu(self.fc9(x))
-        x = x.view(-1, 1, 148)
+        x = torch.sigmoid(self.fc3(x))
+        x = x.view(-1, 148)
         return x
 
 class audio(Dataset):
     def __init__(self, directory):
         self.items = []
 
-        files = [join(directory, f) for f in listdir(directory) if isfile(join(directory, f))]
-        # TODO remove this, just for prototyping
-        files=files[:10]
-        for file in files:
-            with open(file, 'rb') as f:
-                ## NOTE For reconstruction, this data needs to be added to *items*
-                sample = pkl.load(f)
-                #name = sample['name'] # this is needed for reconstruction of the song
-                #name = sample['name']
-                #time = sample['time']
-                window = sample['window']
-                label = sample['label']
-                self.items.append((torch.tensor(np.expand_dims(window, axis=0)).float(), torch.tensor(np.expand_dims(label, axis=0)).float()))
+        #print("{}".format(listdir(directory)))
+        for f in listdir(directory):
+            #print("{}".format(listdir(join(directory, f))))
+            for file in listdir(join(directory, f)):
+                #if isfile(join(directory, f)):
+                #print("{}".format(join(directory, f, file)))
+                self.items.append(join(directory, f, file))
                             
     def __getitem__(self, index):
-        return self.items[index]
+        file = self.items[index]
+        with open(file, 'rb') as f:
+            sample = pkl.load(f)
+            window = sample['window']
+            label = sample['label']
+            return (torch.tensor(np.expand_dims(window, axis=0)).float(), torch.tensor(label).long())
 
     def __len__(self):
         return len(self.items)
 
-
 def main(cuda=torch.cuda.is_available(), gpu=0):
     device = torch.device('cuda:' + str(gpu) if cuda else "cpu")
     print(device)
-    data = audio('./samples_window')
+    data = audio("clone_hero_data/output")
     print(len(data))
 
-    kwargs = {'num_workers': 4, 'pin_memory': True} if cuda else {}
-    data_loader = torch.utils.data.DataLoader(data,batch_size=2,shuffle=True, **kwargs)
+    kwargs = {'num_workers': 8, 'pin_memory': True} if cuda else {}
+    data_loader = torch.utils.data.DataLoader(data, batch_size=256, shuffle=True, **kwargs)
 
     model = network().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
-    criterion = nn.MSELoss(reduction='sum')
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+    criterion = nn.CrossEntropyLoss()
 
-    for epoch in range(10):
+    for epoch in range(20):
         model.train()
+        test_loss = 0
         for batch_idx, (data, target), in enumerate(data_loader):
             data, target = data.to(device), target.to(device)
             optimizer.zero_grad()
             output = model(data)
-            loss = criterion(output, target)
+            loss = criterion(output, torch.max(target, 1)[1])
+            #test_loss += loss
+            print('Epoch', epoch, 'Batch', batch_idx, '/', len(data_loader), 'Loss', loss.cpu().detach().numpy())
             loss.backward()
             optimizer.step()
         
-        test_loss = 0
-        model.eval()
-        for batch_idx, (data, target), in enumerate(data_loader):
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += criterion(output, target)
+        # test_loss = 0
+        # model.eval()
+        # for batch_idx, (data, target), in enumerate(data_loader):
+        #     data, target = data.to(device), target.to(device)
+        #     output = model(data)
+        #     test_loss += criterion(output, target)
 
-        print('Epoch:', epoch, 'Loss:', test_loss)
+        # print('Epoch:', epoch, 'Loss:', test_loss)
+
+        #print('Epoch', epoch, 'Loss', test_loss.cpu().detach().numpy())
+
+        torch.save(model.state_dict(), 'clone_hero_data/models/model' + str(gpu) + '_' + str (epoch) + '.pt')
 
 if __name__ == "__main__":
-    main(cuda=False)
+    main(cuda=True)
